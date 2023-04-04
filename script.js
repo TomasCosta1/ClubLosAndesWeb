c83e46cba8d522aafc8748cacdc5c50ef1ae865a
TomasCosta1 <totocosta3@gmail.com>
2023-04-03 22:31:12 -0300

    titulos sobre img y arreglos

--- script.js
+++ script.js
@@ -8,3 +8,22 @@ window.addEventListener('scroll', function() {
       image.classList.remove('slide-in');
     }
   });
+
+
+  // sistema de bloqueo de superposicion en burger menu
+  let btnBurgerMenuOpen = document.getElementById('btnBurgerMenuOpen');
+  let btnBurgerMenuClose = document.getElementById('btnBurgerMenuClose');
+
+  btnBurgerMenuOpen.addEventListener('click', function(){
+    let main = document.querySelector('main');
+    let footer = document.querySelector('footer');
+    main.style.display = 'none';
+    footer.style.display = 'none'
+  })
+
+  btnBurgerMenuClose.addEventListener('click', function(){
+    let main = document.querySelector('main');
+    let footer = document.querySelector('footer');
+    main.style.display = 'inline'
+    footer.style.display = 'inline'
+  })
